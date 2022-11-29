2b9a3068e5b2f33c3a6924e49416b31217c9bc5c
minjoo <pmj4933@meplz.co.kr>
2022-11-29 09:36:56 +0900

    feat: todo list

--- js/background.js
+++ js/background.js
@@ -4,6 +4,6 @@ const choseImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
-bgImage.src = `img/${choseImage}`;
+bgImage.src = `images/${choseImage}`;
 
 document.body.appendChild(bgImage);
